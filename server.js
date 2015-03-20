ce8b1c92f16a21deed402998c9ae27c247752096
toinnem <anthony.meirlaen@gmail.com>
2015-03-20 23:47:53 +0100

    Express framework

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var util = require('util'),
-    http = require('http'),
-    fs = require('fs');
+var express = require('express'),
+    util = require('util');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8000);
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view options', { layout: false }); // NOTE: express3 remove
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-/* server started */
+app.listen(8000);
 util.puts('> geboekt.gent running on port 8000');
