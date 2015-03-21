8fb9157d1a5b87c53e1d399285d93d084d2c6395
toinnem <anthony.meirlaen@gmail.com>
2015-03-21 01:04:47 +0100

    boere

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,21 @@ set :deploy_to, "/var/www/geboekt"
 set :user, "root"
 
 role :app, "hackdebib.wbdrp.be"
+
+namespace :node do
+    desc "Start the node application"
+    task :start do
+      sudo "forever start server.js"
+    end
+
+    desc "Stop the node application"
+    task :stop do
+      sudo "forever stop server.js"
+    end
+
+    desc "Restart the node application"
+    task :restart do
+      sudo "stop #{upstart_job_name}; true"
+      sudo "start #{upstart_job_name}"
+    end
+end
