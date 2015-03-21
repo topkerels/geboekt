bc29c24d295089dbc79260db003d4224255294f2
toinnem <anthony.meirlaen@gmail.com>
2015-03-21 12:57:43 +0100

    data

--- server.js
+++ server.js
@@ -3,6 +3,37 @@ var express = require('express'),
 
 var app = express();
 
+var banners = {
+    0: {
+        name: 'anthony',
+        title: 'title-anthony.png'
+    },
+    1: {
+        name: 'pieter',
+        title: 'title-pieter.png'
+    }
+};
+
+var books = {
+    0: {
+        title: 'Een dag in Gent',
+        background: 'bg-herman.png',
+        cover: 'cover.png',
+        author: 'Herman Brusselmans',
+        short: '',
+        long: ''
+    }
+};
+
+var pickRandomProperty = function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+            result = prop;
+    return result;
+};
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('view options', { layout: false }); // NOTE: express3 remove
@@ -14,7 +45,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/app', function (req, res) {
-    res.render('app');
+    console.log(pickRandomProperty(banners));
+    res.render('app', {
+        banner : banners[pickRandomProperty(banners)],
+        book : banners[pickRandomProperty(books)]
+    });
 });
 
 app.listen(8000);
